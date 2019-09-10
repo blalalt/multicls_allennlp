1e63681046cdc6180f13baec141a0d422a7a5152
blalalt <blalalt@163.com>
2019-09-10 19:19:38 +0800

    save

--- settings.py
+++ settings.py
@@ -4,12 +4,12 @@ from pathlib import Path
 class Config:
     def __init__(self):
         self.data_home = Path('.') / 'dataset'
-        self.max_seq_len = 1000
+        self.max_seq_len = 512
         self.embedder = 'elmo'
-        self.batch_size = 8
+        self.batch_size = 128
         self.lstm_hid_size = 200
-        self.lr = 0.01
-        self.epochs = 20
+        self.lr = 0.004
+        self.epochs = 30
         self.threshold = 0.5
         self.dropout = 0.3
 
